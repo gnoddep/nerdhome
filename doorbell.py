0f36e9f7f04ee352612bb53b559561e31dda85f7
Peter Gnodde <peter@gnodde.org>
2018-03-28 13:42:29 +0200

    Remove the use of globals, by making the application a class

--- doorbell.py
+++ doorbell.py
@@ -10,77 +10,75 @@ from argparse import ArgumentParser
 import RPi.GPIO as GPIO
 from Nerdman.Button import Button
 
-DOORBELL_DOWNSTAIRS = 18
-DOORBELL_UPSTAIRS = 15
+class Doorbell:
+    DOORBELL_DOWNSTAIRS = 18
+    DOORBELL_UPSTAIRS = 15
 
-wait_mutex = threading.Event()
-influxdb = None
-fqdn = getfqdn()
+    def __init__(self):
+        self.wait_mutex = threading.Event()
+        signal.signal(signal.SIGINT, self._signal_handler)
+        self.influxdb = None
+        self.fqdn = getfqdn()
 
-def main():
-    global influxdb
+    def run(self):
+        parser = ArgumentParser(description='Read data from the doorbell relays send it to influxdb')
+        parser.add_argument('-i', '--hostname', action='store', default='localhost', dest='hostname')
+        parser.add_argument('-d', '--database', action='store', default='doorbell', dest='database')
 
-    parser = ArgumentParser(description='Read data from the doorbell relays send it to influxdb')
-    parser.add_argument('-i', '--hostname', action='store', default='localhost', dest='hostname')
-    parser.add_argument('-d', '--database', action='store', default='doorbell', dest='database')
+        argv = parser.parse_args()
 
-    argv = parser.parse_args()
+        GPIO.setmode(GPIO.BOARD)
 
-    GPIO.setmode(GPIO.BOARD)
+        doorbellDownstairs = Button(self.DOORBELL_DOWNSTAIRS, name='intercom', bouncetime=100)
+        doorbellDownstairs.on_changed(self._handle_doorbell)
 
-    doorbellDownstairs = Button(DOORBELL_DOWNSTAIRS, name='intercom', bouncetime=100)
-    doorbellDownstairs.on_changed(handle_doorbell)
+        doorbellUpstairs = Button(self.DOORBELL_UPSTAIRS, name='door', bouncetime=100)
+        doorbellUpstairs.on_changed(self._handle_doorbell)
 
-    doorbellUpstairs = Button(DOORBELL_UPSTAIRS, name='door', bouncetime=100)
-    doorbellUpstairs.on_changed(handle_doorbell)
+        try:
+            self.influxdb = InfluxDBClient(argv.hostname, database=argv.database)
+            self.influxdb.create_database(argv.database)
 
-    influxdb = InfluxDBClient(argv.hostname, database=argv.database)
-    influxdb.create_database(argv.database)
+            print("Press CTRL+C to exit")
+            while not self.wait_mutex.wait():
+                pass
 
-    try:
-        print("Press CTRL+C to exit")
-        while not wait_mutex.wait():
+            return
+        except KeyboardInterrupt:
             pass
-        return
-    except KeyboardInterrupt:
-        pass
-    finally:
-        GPIO.cleanup()
+        finally:
+            GPIO.cleanup()
 
-    sys.exit(0)
+        sys.exit(0)
 
-def handle_doorbell(button):
-    timestamp = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')
-    state = button.button_state()
-    send_to_influxdb(button.name(), state)
-    print(timestamp, 'The', button.name(), 'doorbell is', 'pressed' if state else 'released')
+    def _handle_doorbell(self, button):
+        timestamp = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')
+        state = button.button_state()
+        self._send_to_influxdb(button.name(), state)
+        print(timestamp, 'The', button.name(), 'doorbell is', 'pressed' if state else 'released')
 
-def signal_handler(signal, frame):
-    global wait_mutex
-    wait_mutex.set()
+    def _signal_handler(self, signal, frame):
+        self.wait_mutex.set()
 
-def send_to_influxdb(doorbell, state):
-    global influxdb
+    def _send_to_influxdb(self, doorbell, state):
+        timestamp = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
 
-    timestamp = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
-
-    data = [
-        {
-            'measurement': 'doorbell',
-            'tags': {
-                'host': fqdn,
-                'doorbell': doorbell,
+        data = [
+            {
+                'measurement': 'doorbell',
+                'tags': {
+                    'host': self.fqdn,
+                    'doorbell': doorbell,
+                },
+                'time': timestamp,
+                'fields': {
+                    'state': state,
+                },
             },
-            'time': timestamp,
-            'fields': {
-                'state': state,
-            },
-        },
-    ]
+        ]
 
-    influxdb.write_points(data, time_precision='u')
+        self.influxdb.write_points(data, time_precision='u')
 
 if __name__ == '__main__':
-    signal.signal(signal.SIGINT, signal_handler)
-    main()
+    Doorbell().run()
 
