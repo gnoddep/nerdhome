66ec705a805dba9c507aac2dff2bbd708ffbd545
Peter Gnodde <peter@gnodde.org>
2018-03-28 14:00:19 +0200

    Send messages to a MQTT broker, instead of directly logging to influxdb

--- doorbell.py
+++ doorbell.py
@@ -1,83 +1,63 @@
 #!/usr/bin/env python3
 
+import sys
 import signal
 import threading
 from datetime import datetime
-from influxdb import InfluxDBClient
-from socket import getfqdn
+from time import time
 from argparse import ArgumentParser
+from paho.mqtt.client import Client as MqttClient
 
 import RPi.GPIO as GPIO
 from Nerdman.Button import Button
 
 class Doorbell:
-    DOORBELL_DOWNSTAIRS = 18
-    DOORBELL_UPSTAIRS = 15
+    DOORBELL_INTERCOM = 18
+    DOORBELL_DOOR = 15
 
     def __init__(self):
         self.wait_mutex = threading.Event()
         signal.signal(signal.SIGINT, self._signal_handler)
-        self.influxdb = None
-        self.fqdn = getfqdn()
+        self.mqtt = MqttClient()
 
     def run(self):
-        parser = ArgumentParser(description='Read data from the doorbell relays send it to influxdb')
-        parser.add_argument('-i', '--hostname', action='store', default='localhost', dest='hostname')
-        parser.add_argument('-d', '--database', action='store', default='doorbell', dest='database')
-
+        parser = ArgumentParser(description='Read data from the doorbell relays send it to MQTT')
+        parser.add_argument('-m', '--hostname', action='store', default='localhost', dest='hostname')
         argv = parser.parse_args()
 
         GPIO.setmode(GPIO.BOARD)
 
-        doorbellDownstairs = Button(self.DOORBELL_DOWNSTAIRS, name='intercom', bouncetime=100)
-        doorbellDownstairs.on_changed(self._handle_doorbell)
+        door = Button(self.DOORBELL_DOOR, name='door', bouncetime=100)
+        door.on_changed(self._handle_doorbell)
 
-        doorbellUpstairs = Button(self.DOORBELL_UPSTAIRS, name='door', bouncetime=100)
-        doorbellUpstairs.on_changed(self._handle_doorbell)
+        intercom = Button(self.DOORBELL_INTERCOM, name='intercom', bouncetime=100)
+        intercom.on_changed(self._handle_doorbell)
 
         try:
-            self.influxdb = InfluxDBClient(argv.hostname, database=argv.database)
-            self.influxdb.create_database(argv.database)
+            self.mqtt.connect(argv.hostname)
+            self.mqtt.loop_start()
+
+            self._handle_doorbell(door)
 
             print("Press CTRL+C to exit")
             while not self.wait_mutex.wait():
                 pass
-
-            return
         except KeyboardInterrupt:
             pass
         finally:
+            self.mqtt.loop_stop()
+            self.mqtt.disconnect()
             GPIO.cleanup()
 
         sys.exit(0)
 
     def _handle_doorbell(self, button):
-        timestamp = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')
         state = button.button_state()
-        self._send_to_influxdb(button.name(), state)
-        print(timestamp, 'The', button.name(), 'doorbell is', 'pressed' if state else 'released')
+        self.mqtt.publish('doorbell/' + button.name(), str(state) + ':' + str(time()), qos=0)
+        print(datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f'), 'The', button.name(), 'doorbell is', 'pressed' if state else 'released')
 
     def _signal_handler(self, signal, frame):
         self.wait_mutex.set()
-
-    def _send_to_influxdb(self, doorbell, state):
-        timestamp = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
-
-        data = [
-            {
-                'measurement': 'doorbell',
-                'tags': {
-                    'host': self.fqdn,
-                    'doorbell': doorbell,
-                },
-                'time': timestamp,
-                'fields': {
-                    'state': state,
-                },
-            },
-        ]
-
-        self.influxdb.write_points(data, time_precision='u')
 
 if __name__ == '__main__':
     Doorbell().run()
